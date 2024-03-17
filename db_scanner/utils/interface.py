6fe54b193532131cc9220f1165053998f23e2fc9
Musa-Sina-Ertugrul <yutubems@gmail.com>
2024-03-17 16:18:39 +0300

    Adjusted according to TDD #7

--- db_scanner/utils/interface.py
+++ db_scanner/utils/interface.py
@@ -4,12 +4,11 @@ from typing import Any,Self
 class Interface(type):
     
     def __new__(mcls: type[Self], name: str, bases: tuple[type, ...], namespace: dict[str, Any], **kwargs: str) -> Self:
-        
+
         for value in kwargs.values():
-            
             if type(value) is not str:
-                raise TypeError("Wrong interface arfument")
-            elif not hasattr(bases,value) and not callable(getattr(bases,value)):
-                raise NotImplementedError(f"{value} not implemented in {name.__class__.__name__} class")
+                raise TypeError("Wrong interface argument")
+            elif not namespace.get(value,False) or not callable(namespace.get(value,None)):
+                raise NotImplementedError(f"{value} not implemented in {name} class")
         
         return super().__new__(mcls, name, bases, namespace)
